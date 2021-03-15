5d85ee0c250a3a8221376dced4f225d3c1c2ca54
Mohamed Raza <razavistag@outlook.com>
2021-03-15 22:42:22 +0530

    init project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-row>
+      <v-col md="12">hey
+        <v-btn color="red">test</v-btn>
+      </v-col>
+    </v-row>
+    <p class="css_style" >TEST From CSS</p>
+    <p class="text_color">TEST From Scss</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+  },
+  data() {
+    return {
+      companies: [
+        {
+          id: 1,
+          name: "E Corp",
+        },
+        {
+          id: 2,
+          name: "Allsafe",
+        },
+      ],
+    };
+  },
+  mounted(){
+    console.log(this.companies[0].name)
   }
-}
+};
 </script>
+<style lang="scss">
+@import "./style/style.scss";
+
+// $bg-background: #283593;
+// $text-color: #ffffff;
+
+.css_style {
+  background-color: #26a69a;
+  color: #ffffff;
+}
+.text_color {
+  background: $bg-background;
+  color: $text-color;
+}
+</style>
